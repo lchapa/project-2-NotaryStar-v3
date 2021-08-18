e006aedf2f76bc98d3d72680a34e3188a91cea7f
Luis Chapa <luischapam@hotmail.com>
2021-08-18 11:34:49 -0500

    Delete line

--- app/src/index.js
+++ app/src/index.js
@@ -60,6 +60,5 @@ window.addEventListener("load", async function() {
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
   }
-
   App.start();
 });
